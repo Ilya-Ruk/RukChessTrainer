5d1ff2b7baa88e08bf53d4d477245b8394e997cf
Ilya Rukavishnikov <ilya-ndt@mail.ru>
2022-10-03 18:26:06 +0300

    Deleted unused code 1

--- src/trainer.h
+++ src/trainer.h
@@ -11,8 +11,4 @@ INLINE float Error(float r, Board* b) { return powf(fabsf(r - (float)b->wdl / 2.
 
 INLINE float ErrorGradient(float r, Board* b) { return 2.5f * (r - (float)b->wdl / 2.0f) * sqrtf(fabsf(r - (float)b->wdl / 2.0f)); }
 
-//INLINE float Error(float r, Board* b) { return powf(r - (float)b->wdl / 2.0f, 2.0f); }
-
-//INLINE float ErrorGradient(float r, Board* b) { return 2.0f * (r - (float)b->wdl / 2.0f); }
-
 #endif
