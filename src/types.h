fbad7b6389df18f8467052da59b81a6a7eab2840
Ilya Rukavishnikov <ilya-ndt@mail.ru>
2023-04-20 09:54:58 +0100

    Deleted L1 regularization

--- src/types.h
+++ src/types.h
@@ -20,10 +20,6 @@
 #define BETA2 0.999f
 #define EPSILON 1e-8f
 
-// L1 regularization (Lasso regression)
-
-//#define LAMBDA (1.0f / (1024 * 1024))
-
 #define MAX_VALID_POSITIONS 60000000
 #define MAX_TRAIN_POSITIONS 500000000
 
